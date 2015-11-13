60695533a39baad43e2d597a820767df365bae08
Hana Lee <i@leehana.co.kr>
2015-11-13 14:20:47 +0900

    로그인 테스트를 위한 테스트 코드 클래스 추가.

---
+++ src/test/java/kr/co/leehana/LoginTest.java
@@ -0,0 +1,15 @@
+package kr.co.leehana;
+
+import org.junit.Test;
+
+/**
+ * @author Hana Lee
+ * @since 2015-11-13 14-19
+ */
+public class LoginTest {
+
+	@Test
+	public void testLogin() throws Exception {
+
+	}
+}
