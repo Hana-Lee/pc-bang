62e3be0da396c7b48ecd4f5f0534534fa3b65814
Hana Lee <i@leehana.co.kr>
2015-11-14 03:30:11 +0900

    서버와 클라이언트 통신 및 사용자 화면들을 관리 하기 위한 메인 컨트롤 클래스 추가.

---
+++ src/main/java/kr/co/leehana/controller/MainController.java
@@ -0,0 +1,15 @@
+package kr.co.leehana.controller;
+
+import kr.co.leehana.view.LoginFrame;
+import kr.co.leehana.view.ManageView;
+
+/**
+ * @author Hana Lee
+ * @since 2015-11-14 03-28
+ */
+public class MainController {
+
+	private LoginFrame loginFrame;
+	private ManageView manageView;
+
+}
