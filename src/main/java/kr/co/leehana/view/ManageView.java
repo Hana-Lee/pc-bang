e91d1092ac7aaaec0ab5236814437799a1767978
Hana Lee <i@leehana.co.kr>
2015-11-13 22:30:34 +0900

    관리 화면 좌측 상단에 디지털 시계 표시 기능 추가.

--- src/main/java/kr/co/leehana/view/ManageView.java
+++ src/main/java/kr/co/leehana/view/ManageView.java
@@ -2,6 +2,9 @@ package kr.co.leehana.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * @author Hana Lee
@@ -35,8 +38,14 @@ public class ManageView extends JFrame {
 		clockPanel.setOpaque(false);
 		new Thread(clockPanel).start();
 
+		ClockMessage clockMessage = new ClockMessage();
+		clockMessage.setBounds(80, 53, 100, 100);
+		clockMessage.setOpaque(false);
+		new Thread(clockMessage).start();
+
 		layeredPane.add(mainPanel, new Integer(0));
 		layeredPane.add(clockPanel, new Integer(1));
+		layeredPane.add(clockMessage, new Integer(2));
 
 		add(layeredPane);
 
@@ -114,6 +123,51 @@ public class ManageView extends JFrame {
 			}
 		}
 	}
+
+	class ClockMessage extends JPanel implements Runnable {
+
+		private int ampmCode = Calendar.getInstance().get(Calendar.AM_PM);
+		private String[] ampmValues = {"AM", "PM"};
+		private String timeValue = new SimpleDateFormat("hh:mm:ss").format(new Date());
+		private JLabel timeLabel;
+		private JLabel ampmLabel;
+
+		public ClockMessage() {
+			init();
+		}
+
+		private void init() {
+			this.setLayout(null);
+			timeLabel = new JLabel(timeValue);
+			timeLabel.setBounds(0, 0, 100, 20);
+			timeLabel.setForeground(new Color(36, 205, 198));
+			timeLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+
+			ampmLabel = new JLabel(ampmValues[ampmCode]);
+			ampmLabel.setBounds(15, 20, 100, 30);
+			ampmLabel.setForeground(new Color(36, 205, 198));
+			ampmLabel.setFont(new Font("맑은 고딕", Font.BOLD, 12));
+//			ampmLabel.setFont(new Font("배달의민족 한나는 열한살", Font.BOLD, 12));
+//			ampmLabel.setFont(new Font("배달의민족 주아", Font.BOLD, 12));
+
+			add(timeLabel, BorderLayout.NORTH);
+			add(ampmLabel, BorderLayout.CENTER);
+		}
+
+		@Override
+		public void run() {
+			do {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+
+				timeLabel.setText(new SimpleDateFormat("hh:mm:ss").format(new Date()));
+			} while (true);
+		}
+	}
+
 
 	public static void main(String[] args) {
 		new ManageView();
